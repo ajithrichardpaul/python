4010ac1dde0ba5d795c0b457a3256bc8db547d53
ajithrichardpaul <36755392+ajithrichardpaul@users.noreply.github.com>
2018-02-23 15:39:05 +0530

    Create largest of three no.py

---
+++ beginner/largest of three no.py
@@ -0,0 +1,9 @@
+num1=input("enter the 1st no:")
+num2=input("enter the 2nd no:")
+num3=input("enter the 3rd no:")
+if (num1>num2>num3):
+    print("num1 is gratest")
+elif (num2>num1>num3):
+    print("num2 is gratest")
+else:
+    print("num3 is gratest")
